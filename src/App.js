12daa8090306080e966e01bc11c8e7564dfbcfbb
yidahc <yidah.a.curiel@gmail.com>
2019-12-11 21:31:48 -0600

    renderizando mensaje e importando bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+//import Card from './Components/card';
+//import Navbar from './Components/navbar';
+//import Form from './Components/form';
+import axios from 'axios';
 
-function App() {
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state= {
+      albums: [],
+      message: ''
+    }
+  }
+
+  componentDidMount(){
+    axios.get('https://jsonplaceholder.typicode.com/users/1/albums')
+    .then(albums => {
+      this.setState({
+        albums: albums,
+        message: 'listo!'
+      })
+    })
+  }
+
+  render(){  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <h2>
+       { this.state.message }
+     </h2>
     </div>
-  );
+  )};
 }
 
 export default App;
